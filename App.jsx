a2b5bf494ccbdfc8fa219fefa9e8b12aa24658f6
HemasriMurugakani <hemaleena1102@gmail.com>
2024-11-10 09:18:55 +0530

    Initial setup

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centeredView}>
+        <Text style={styles.text}>Hello, React Native!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F4F6',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default App;
